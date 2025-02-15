ad53edef2c2974a87beb445b8c0682ef286f7179
Atiqur Rahman <atiqrs.me@gmail.com>
2025-02-16 04:16:01 +0600

    add new home screen svgs

--- lib/src/commons/resources/assets.dart
+++ lib/src/commons/resources/assets.dart
@@ -10,6 +10,21 @@ class AppAssets {
   static const iconWebsite = '${_pathIcons}website.svg';
   static const iconSocial = '${_pathIcons}social.svg';
 
+  static const iconArrowBack = '${_pathIcons}arrow-back.svg';
+  static const iconArrowRightBack = '${_pathIcons}arrow_right_back.svg';
+  static const iconBatteryFull = '${_pathIcons}battery-full.svg';
+  static const iconCellularNetwork = '${_pathIcons}cellular-network.svg';
+  static const iconChip = '${_pathIcons}chip.svg';
+  static const iconCpu = '${_pathIcons}cpu.svg';
+  static const iconDirection = '${_pathIcons}direction.svg';
+  static const iconFingerprint = '${_pathIcons}finger-access.svg';
+  static const iconHeadphone = '${_pathIcons}headphone.svg';
+  static const iconMenu = '${_pathIcons}menu.svg';
+  static const iconRom = '${_pathIcons}rom.svg';
+  static const iconShare = '${_pathIcons}share.svg';
+  static const iconSimCard = '${_pathIcons}simcard.svg';
+  static const iconSquareArrow = '${_pathIcons}square-arrow.svg';
+
   /// Images
   static const imageDemo = '${_pathImages}demo.png';
 
