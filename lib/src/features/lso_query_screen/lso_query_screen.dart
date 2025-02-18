c658fca280e2634dcc9717317a4f0e9b00bcae43
Atiqur Rahman <atiqrs.me@gmail.com>
2025-02-18 14:29:20 +0600

    add lso remaining item input field and button

--- lib/src/features/lso_query_screen/lso_query_screen.dart
+++ lib/src/features/lso_query_screen/lso_query_screen.dart
@@ -6,9 +6,18 @@ import '../../commons/resources/strings.dart';
 import '../../commons/resources/styles.dart';
 import '../../commons/utils/custom_app_bar.dart';
 import '../../commons/widgets/common/color_round_card_widget.dart';
+import '../../commons/widgets/common/rounded_text_button.dart';
 
-class LsoQueryScreen extends StatelessWidget {
+class LsoQueryScreen extends StatefulWidget {
   const LsoQueryScreen({super.key});
+
+  @override
+  State<LsoQueryScreen> createState() => _LsoQueryScreenState();
+}
+
+class _LsoQueryScreenState extends State<LsoQueryScreen> {
+  final FocusNode lsoFocusNode = FocusNode();
+  final TextEditingController lsoController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -43,6 +52,44 @@ class LsoQueryScreen extends StatelessWidget {
                     style: AppStyles.gray16W500TextStyle,
                     overflow: TextOverflow.ellipsis,
                   ),
+                  SizedBox(height: AppDimens.spacing10),
+                  TextFormField(
+                    focusNode: lsoFocusNode,
+                    controller: lsoController,
+                    onChanged: (value) {},
+                    decoration: InputDecoration(
+                      hintText: AppStrings.enterLSOHints,
+                      hintStyle: AppStyles.gray14W500TextStyle,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: AppColors.borderLightWhite02Color, width: AppDimens.width1_5),
+                        borderRadius: BorderRadius.all(Radius.circular(AppDimens.radius16)),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: AppColors.borderLightWhite02Color, width: AppDimens.width2),
+                        borderRadius: BorderRadius.all(Radius.circular(AppDimens.radius16)),
+                      ),
+                      contentPadding: EdgeInsets.all(AppDimens.spacing16),
+                    ),
+                    style: AppStyles.white14W500TextStyle,
+                  ),
+                  SizedBox(height: AppDimens.spacing24),
+                  Row(
+                    children: [
+                      RoundedTextButton(
+                        onPressed: () {},
+                        style: CustomButtonStyle.redBig,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: AppDimens.spacing16, vertical: AppDimens.spacing12),
+                          child: Text(
+                            AppStrings.getInformation,
+                            style: AppStyles.white16W500TextStyle,
+                          ),
+                        ),
+                      ),
+                      Spacer(),
+                    ],
+                  )
                 ],
               ),
             ),
