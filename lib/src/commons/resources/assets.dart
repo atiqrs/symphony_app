e63f1fd0c75e84aba50fff496108f4e09c3b3b96
Atiqur Rahman <atiqrs.me@gmail.com>
2025-02-18 14:50:53 +0600

    update icons and images naming convention which are skipped

--- lib/src/commons/resources/assets.dart
+++ lib/src/commons/resources/assets.dart
@@ -10,25 +10,24 @@ class AppAssets {
   static const iconWebsite = '${_pathIcons}website.svg';
   static const iconSocial = '${_pathIcons}social.svg';
 
-  static const iconArrowBack = '${_pathIcons}arrow-back.svg';
+  static const iconArrowBack = '${_pathIcons}arrow_back.svg';
   static const iconArrowRightBack = '${_pathIcons}arrow_right_back.svg';
-  static const iconCellularNetwork = '${_pathIcons}cellular-network.svg';
+  static const iconCellularNetwork = '${_pathIcons}cellular_network.svg';
   static const iconRam = '${_pathIcons}ram.svg';
   static const iconCpu = '${_pathIcons}cpu.svg';
   static const iconDirection = '${_pathIcons}direction.svg';
-  static const iconFingerprint = '${_pathIcons}finger-access.svg';
+  static const iconFingerprint = '${_pathIcons}fingerprint_access.svg';
   static const iconHeadphone = '${_pathIcons}headphone.svg';
   static const iconMenu = '${_pathIcons}menu.svg';
   static const iconRom = '${_pathIcons}rom.svg';
   static const iconShare = '${_pathIcons}share.svg';
-  static const iconSimCard = '${_pathIcons}simcard.svg';
-  static const iconSquareArrow = '${_pathIcons}square-arrow.svg';
+  static const iconSimCard = '${_pathIcons}sim_card.svg';
   static const iconPattern = '${_pathIcons}pattern.svg';
   static const iconMobile = '${_pathIcons}mobile.svg';
   static const iconBattery = '${_pathIcons}battery.svg';
   static const iconCamera = '${_pathIcons}camera.svg';
   static const iconStorage = '${_pathIcons}storage.svg';
-  static const iconExtendableSquare = '${_pathIcons}square-arrow.svg';
+  static const iconExtendableSquare = '${_pathIcons}square_arrow.svg';
 
   static const iconAppointment = '$_pathIcons/support/appointment.svg';
   static const iconComment = '$_pathIcons/support/comment.svg';
@@ -38,10 +37,10 @@ class AppAssets {
   static const iconList = '$_pathIcons/support/list.svg';
 
   /// Images
-  static const imagePhoneSpec = '${_pathImages}phone-spec.png';
+  static const imagePhoneSpec = '${_pathImages}phone_spec.png';
   static const imagePhone = '${_pathImages}phone.png';
   static const imageWatch = '${_pathImages}watch.png';
-  static const imageTypeC = '${_pathImages}type-c.png';
+  static const imageTypeC = '${_pathImages}type_c.png';
   static const imageEntertainOne = '${_pathImages}entertain_one.png';
 
   static const imageGameOne = '${_pathImages}game_one.png';
